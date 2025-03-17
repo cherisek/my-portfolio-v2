118a300c5d49939f5db2859576228228759e80c3
Cherise Knox <cheriseknox@Mac.attlocal.net>
2025-03-17 14:51:02 -0400

    added link for iab app

--- src/components/Portfolio/Iab/Iab.jsx
+++ src/components/Portfolio/Iab/Iab.jsx
@@ -11,7 +11,14 @@ export const Iab = () => {
           <p>
             For this website, I used React, React Hooks, Javascript, HTML, and CSS. I worked on the project individually and build wireframes using sketch. I used React because my company at the time required us to use React. As well as React, makes it easy to reuse components and be able to make use of the virtual DOM for faster reflection of updates.   
           </p>
-          <button className="view-project border-button">View Project</button>
+          <a 
+            href="https://iab.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="view-project border-button"
+          >
+            View Project
+          </a>
         </div>
         <div className="lines"></div>
       </div>
